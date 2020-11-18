4f6b531522d4a71889bfa615162e0b1f632ae51e
Nino Matassa <nino.matassa@gmail.com>
2020-11-18 07:03:05 +0000

    Greek Alphabet

---
+++ app/src/main/java/ie/webware/algebra/Constants.java
@@ -0,0 +1,62 @@
+package ie.webware.algebra;
+
+//https://unicode-table.com/en/sets/mathematical-signs/
+public class Constants
+{
+  public static final String integral = "∫";
+  public static final String forAll = "∀";
+  public static final String exists = "∃";
+  public static final String notExists = "∄";
+  public static final String sqRoot = "√";
+  public static final String infinity = "∞";
+  // Greek Letters
+  public static final String Alpha = "Α";
+  public static final String alpha = "α";
+  public static final String Beta = "Β";
+  public static final String beta = "β";
+  public static final String Gamma = "Γ";
+  public static final String gamma = "γ";
+  public static final String Delta = "Δ";
+  public static final String delta = "δ";
+  public static final String Epsilon = "Ε";
+  public static final String epsilon = "ε";
+  public static final String Zeta = "Ζ";
+  public static final String zeta = "ζ";
+  public static final String Eta = "Η";
+  public static final String eta = "η";
+  public static final String Theta = "Θ";
+  public static final String theta = "θ";
+  public static final String Iota = "Ι";
+  public static final String iota = "ι";
+  public static final String Kappa = "Κ";
+  public static final String kappa = "κ";
+  public static final String Lamda = "Λ";
+  public static final String lamda = "λ";
+  public static final String Mu = "Μ";
+  public static final String mu = "μ";
+  public static final String Nu = "N";
+  public static final String nu = "ν";
+  public static final String Ki = "Ξ";
+  public static final String ki = "ξ";
+  public static final String Omicron = "Ο";
+  public static final String omicron = "ο";
+  public static final String Pi = "Π";
+  public static final String pi = "π";
+  public static final String Rho = "Ρ";
+  public static final String rho = "ρ";
+  public static final String Sigma = "Σ";
+  public static final String sigma = "σ";
+  public static final String finalSigma = "ς";
+  public static final String Tau = "Τ";
+  public static final String tau = "τ";
+  public static final String Upsilon = "Υ";
+  public static final String upsilon = "υ";
+  public static final String Phi = "Φ";
+  public static final String phi = "φ";
+  public static final String Chi = "Χ";
+  public static final String chi = "χ";
+  public static final String Psi = "Ψ";
+  public static final String psi = "ψ";
+  public static final String Omega = "Ω";
+  public static final String omega = "ω";
+}
--- app/src/main/java/ie/webware/algebra/MainActivity.java
+++ app/src/main/java/ie/webware/algebra/MainActivity.java
@@ -2,6 +2,10 @@ package ie.webware.algebra;
 
 import android.app.*;
 import android.os.*;
+import android.view.*;
+import android.widget.*;
+import java.nio.charset.*;
+import java.io.*;
 
 public class MainActivity extends Activity 
 {
@@ -10,5 +14,12 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        TextView textView = findViewById(R.id.textId);
+        textView.setText(Constants.Sigma);
+        
     }
 }
+
+/*
+
+*/
